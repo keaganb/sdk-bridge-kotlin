7a1848461506a50ec57441e3afc7399b2e936812
Carter Harrison <carterjharrison@gmail.com>
2018-10-22 12:51:34 -0700

    correct alternation

--- src/main/java/network/xyo/sdkbridgekotlin/XyoBridge.kt
+++ src/main/java/network/xyo/sdkbridgekotlin/XyoBridge.kt
@@ -23,22 +23,21 @@ open class XyoBridge (private val bridgeFromNetwork : XyoNetworkProviderInterfac
 
     private var index = 0
     private var isBridging = true
-
-
     override val procedureCatalogue = XyoBridgeCollectorProcedureCatalogue()
 
-    /**
-     * The problem is that the whoToTalkTo is a getter and it can get to the bottom
-     */
     override suspend fun findSomeoneToTalkTo() = suspendCoroutine<XyoNetworkPipe> { cont ->
         var bridgeFromFinder : Deferred<XyoNetworkPipe?>? = null
         var bridgeToFinder : Deferred<XyoNetworkPipe?>? = null
         var bridgeFromFinderHandler : Deferred<Unit>?= null
         var bridgeToFinderHandler : Deferred<Unit>? = null
 
-        bridgeFromFinder = bridgeFromNetwork.find(procedureCatalogue)
         bridgeFromFinderHandler = GlobalScope.async {
-            val con = bridgeFromFinder.await()!!
+            if (index % WHEN_TO_FORCE_BRIDGE == 0 && isBridging) {
+                delay(PRIORITY_HEAD_START)
+            }
+
+            bridgeFromFinder = bridgeFromNetwork.find(procedureCatalogue)
+            val con = bridgeFromFinder?.await()!!
             index++
 
             bridgeFromNetwork.stop()
@@ -46,30 +45,39 @@ open class XyoBridge (private val bridgeFromNetwork : XyoNetworkProviderInterfac
 
             cont.resume(con)
             bridgeToFinder?.cancel()
-            bridgeFromFinder.cancel()
+            bridgeToFinder?.cancelChildren()
+            bridgeFromFinder?.cancel()
+            bridgeFromFinder?.cancelChildren()
+            bridgeToFinderHandler?.cancelChildren()
             bridgeToFinderHandler?.cancel()
             bridgeFromFinderHandler?.cancel()
+            bridgeFromFinderHandler?.cancelChildren()
             coroutineContext.cancel()
             return@async
         }
 
-
-        if (index % WHEN_TO_BRIDGE == 0 && isBridging) {
-            bridgeToFinder = bridgeToNetwork.find(procedureCatalogue)
+        if (isBridging) {
             bridgeToFinderHandler = GlobalScope.async {
-                delay((PRIORITY_HEAD_START * Math.random()).toInt())
+                if (index % WHEN_TO_FORCE_BRIDGE != 0) {
+                    delay((PRIORITY_HEAD_START))
+                }
 
-                val con = bridgeToFinder.await()!!
+                bridgeToFinder = bridgeToNetwork.find(procedureCatalogue)
+                val con = bridgeToFinder?.await()!!
                 index++
 
                 bridgeFromNetwork.stop()
                 bridgeToNetwork.stop()
 
                 cont.resume(con)
-                bridgeFromFinder.cancel()
-                bridgeToFinder.cancel()
+                bridgeFromFinder?.cancel()
+                bridgeFromFinder?.cancelChildren()
+                bridgeToFinder?.cancel()
+                bridgeToFinder?.cancelChildren()
+                bridgeFromFinderHandler.cancelChildren()
                 bridgeFromFinderHandler.cancel()
                 bridgeToFinderHandler?.cancel()
+                bridgeToFinderHandler?.cancelChildren()
                 coroutineContext.cancel()
                 return@async
             }
@@ -82,7 +90,7 @@ open class XyoBridge (private val bridgeFromNetwork : XyoNetworkProviderInterfac
     }
 
     companion object {
-        const val WHEN_TO_BRIDGE = 3
+        const val WHEN_TO_FORCE_BRIDGE = 10
         const val PRIORITY_HEAD_START = 60_000
     }
 }
